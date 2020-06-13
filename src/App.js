733847dca5bc1a7488c5858778b677175916a102
Llewellyn Thomas <llewellynthomas@mac.com>
2020-06-13 21:31:14 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React again here 2
         </a>
       </header>
     </div>
